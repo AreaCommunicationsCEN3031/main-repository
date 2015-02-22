c35eacf3bc3e089d96aaef0fba25c8b039c77844
gduarte93 <gduarte93@aol.com>
2015-02-22 15:49:34 -0500

    check if js works

--- config/env/all.js
+++ config/env/all.js
@@ -62,7 +62,8 @@ module.exports = {
 			'public/config.js',
 			'public/application.js',
 			'public/modules/*/*.js',
-			'public/modules/*/*[!tests]*/*.js'
+			'public/modules/*/*[!tests]*/*.js',
+			'public/modules/core/views/main_js'
 		],
 		tests: [
 			'public/lib/angular-mocks/angular-mocks.js',
